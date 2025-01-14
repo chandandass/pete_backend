13c0751631946003c6daabf78d562dc17d8ae85d
chandandass <cdas99633@gmail.com>
2025-01-14 18:59:58 +0530

    modules created for user, children, timeline

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { ChildrenModule } from './modules/children/children.module';
+import { TimelineModule } from './modules/timeline/timeline.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, ChildrenModule, TimelineModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
