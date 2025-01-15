e737b756c01116b7ba387cd793ecf2ab24e83faf
chandandass <cdas99633@gmail.com>
2025-01-15 14:56:52 +0530

    user controller & validation

--- src/modules/user/dto/user.dto.ts
+++ src/modules/user/dto/user.dto.ts
@@ -1,26 +1,106 @@
-export class SignUpDto {
+import { Exclude } from 'class-transformer';
+import {
+  IsString,
+  IsArray,
+  IsDateString,
+  IsNumber,
+  IsIn,
+} from 'class-validator';
+
+// Shared DTO for Kid
+export class KidDto {
+  @IsString()
   name: string;
+
+  @IsDateString()
+  dateOfBirth: string;
+
+  @IsString()
+  gender: string;
+}
+
+// Base User DTO
+export class UserDto {
+  @IsNumber()
+  id: number; // id will be required in UserDto
+
+  @IsString()
+  name: string;
+
+  @IsString()
+  relation: string;
+
+  @IsString()
   email: string;
+}
+
+// ReminderSchedules DTO with class-validator
+export class ReminderSchedulesDto {
+  @IsString()
+  update: string;
+
+  @IsString()
+  unanswered: string;
+
+  @IsString()
+  random: string;
+}
+
+// SignUp DTO (used for user registration)
+export class SignUpDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  email: string;
+
+  @IsString()
   password: string;
-  kids: KidDto[];
+
+  @IsArray()
+  kids: KidDto[]; // Array of kids, validated as an array of KidDto objects
 }
 
+// Login DTO
 export class LoginDto {
-  public email: string;
-  public password: string;
+  @IsString()
+  email: string;
+
+  @IsString()
+  password: string;
 }
 
+// Update Details DTO (used for updating user details)
 export class UpdateDetailsDto {
-  public name: string;
-  public relation: string;
-  public email: string;
+  @IsString()
+  name: string;
+
+  @IsIn(['MOM', 'DAD']) // Ensures the value is either 'MOM' or 'DAD'
+  relation: 'MOM' | 'DAD';
+
+  @IsString()
+  email: string;
 }
 
+// Update Password DTO
 export class UpdatePasswordDto {
+  @IsString()
   currentPassword: string;
+
+  @IsString()
   newPassword: string;
 }
 
+// CurrentUserResponse DTO extending UserDto but excluding `id`
+export class CurrentUserResponse extends UserDto {
+  @Exclude() // Exclude the `id` field in the CurrentUserResponse response
+  id: number;
+
+  kids: KidDto[];
+  reminderSchedules: ReminderSchedulesDto;
+}
+
+// Auth Response DTO
 export class AuthResponse {
   constructor(
     public message: string,
@@ -28,6 +108,7 @@ export class AuthResponse {
   ) {}
 }
 
+// Update User Response DTO
 export class UpdateUserResponse {
   constructor(
     public message: string,
@@ -39,13 +120,7 @@ export class UpdateUserResponse {
   ) {}
 }
 
+// Action Response DTO
 export class ActionResponse {
   constructor(public message: string) {}
-}
-
-//shared
-export class KidDto {
-  name: string;
-  dateOfBirth: string;
-  gender: string;
 }
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,49 +1,79 @@
 import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
-import { 
-  SignUpDto, 
-  LoginDto, 
-  UpdateDetailsDto, 
-  UpdatePasswordDto, 
-  AuthResponse, 
-  UpdateUserResponse, 
-  ActionResponse 
+import {
+  SignUpDto,
+  LoginDto,
+  UpdateDetailsDto,
+  UpdatePasswordDto,
+  AuthResponse,
+  UpdateUserResponse,
+  ActionResponse,
+  CurrentUserResponse,
+  UserDto,
 } from './dto/user.dto';
 
 @Controller('user')
 export class UserController {
+  @Get('current')
+  async getCurrentUser(): Promise<CurrentUserResponse> {
+    const dummyResponse: CurrentUserResponse = {
+      name: 'Neel Patel',
+      relation: 'MOM',
+      email: 'neelpatel.6531@gmail.com',
+      kids: [
+        {
+          name: 'Emli',
+          dateOfBirth: '2015-06-25',
+          gender: 'FEMALE',
+        },
+      ],
+      reminderSchedules: {
+        update: '7:00',
+        unanswered: '18:00',
+        random: '8:00',
+      },
+      id: -1,
+    };
+    return dummyResponse;
+  }
 
-    @Get("current")
-    async getCurrentUser() {
-        // Add logic to get current user
-    }
+  @Post('sign-up')
+  async singUp(@Body() userData: SignUpDto): Promise<AuthResponse> {
+    console.log(userData.name.toLocaleLowerCase());
+    console.log('userData', userData);
+    return new AuthResponse('sing-up', '1234');
+  }
 
-    @Post('sign-up')
-    async singUp(@Body() userData: SignUpDto): Promise<AuthResponse> {
-        console.log(userData.name.toLocaleLowerCase());
-        console.log("userData", userData);
-        return new AuthResponse("error", '1234');
-    }
+  @Post('login')
+  async login(@Body() userData: LoginDto): Promise<AuthResponse> {
+    console.log(userData.email.toLocaleLowerCase());
+    console.log('userData', userData);
+    console.log('typeofpass', typeof userData.password);
+    return new AuthResponse('login', '1234');
+  }
 
-    @Post('login')
-    async login(@Body() userData: LoginDto): Promise<AuthResponse> {
-        console.log(userData.email.toLocaleLowerCase());
-        console.log("userData", userData);
-        return new AuthResponse("error", '1234');
-    }
+  @Put('details')
+  async updateDetails(
+    @Body() userData: UpdateDetailsDto,
+  ): Promise<UpdateUserResponse> {
+    console.log(userData);
+    return new UpdateUserResponse('details', {
+      name: '',
+      email: '',
+      relation: '',
+    });
+  }
 
-    @Put('details')
-    async updateDetails(@Body() userData: UpdateDetailsDto): Promise<UpdateUserResponse> {
-        console.log(userData)
-        return new UpdateUserResponse("", {name: '', email: '', relation: ''});
-    }
+  @Put('password')
+  async updatePassword(
+    @Body() passwords: UpdatePasswordDto,
+  ): Promise<ActionResponse> {
+    console.log(passwords);
+    return new ActionResponse('updatePassword');
+  }
 
-    @Put('password')
-    async updatePassword(@Body() passwords: UpdatePasswordDto): Promise<ActionResponse> {
-        return new ActionResponse("")
-    }
-
-    @Delete('delete')
-    async delete(): Promise<ActionResponse> {
-        return new ActionResponse("")
-    }
+  @Delete('delete')
+  async delete(@Body() userData: UserDto): Promise<ActionResponse> {
+    console.log(userData);
+    return new ActionResponse('deleteUser');
+  }
 }
