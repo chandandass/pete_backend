cb576aebc6754caba029e2a5c82417d1bbc4a3bc
chandandass <cdas99633@gmail.com>
2025-01-20 04:36:24 +0530

    prompt generator implemented

---
+++ src/modules/prompt/handler.service.ts
@@ -0,0 +1,170 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Post } from 'src/entities/post.entity';
+import { StaticFamilyPrompt } from 'src/entities/static/static-family-prompt.entity';
+import { StaticReflectionPrompt } from 'src/entities/static/static-reflection-prompt.entity';
+import { TrackReflectionPrompt } from 'src/entities/track_reflection_prompt.entity';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
+import { PromptGenerator } from './generator.service';
+import { StaticKidPrompt } from 'src/entities/static/static-kid-mvp-prompt.entity';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PromptHandler {
+  private static readonly EXPIRATION_DAYS = 21;
+
+  constructor(
+    @InjectRepository(TrackReflectionPrompt)
+    private readonly trp_repository: Repository<TrackReflectionPrompt>,
+    @InjectRepository(StaticReflectionPrompt)
+    private readonly srp_repository: Repository<StaticReflectionPrompt>,
+    @InjectRepository(StaticFamilyPrompt)
+    private readonly sfp_repository: Repository<StaticFamilyPrompt>,
+    @InjectRepository(StaticKidPrompt)
+    private readonly skp_repository: Repository<StaticKidPrompt>,
+  ) {}
+
+  // Generalized method for fetching static prompts
+  private async getPrompts<T extends { id: number }>(
+    repository: Repository<T>,
+    user: User,
+    generatorMethod: (user: User, prompts: T[]) => Post[],
+  ): Promise<Post[]> {
+    const prompts = await repository.find();
+    return generatorMethod(user, prompts);
+  }
+
+  private async getFamilyPrompts(user: User): Promise<Post[]> {
+    return this.getPrompts(
+      this.sfp_repository,
+      user,
+      PromptGenerator.generateFamilyPrompts,
+    );
+  }
+
+  public async getChildrenPrompts(user: User): Promise<Post[]> {
+    return this.getPrompts(
+      this.skp_repository,
+      user,
+      PromptGenerator.generateKidPrompts,
+    );
+  }
+
+  private async getNextReflectionPrompt(
+    currentPromptId: number,
+  ): Promise<StaticReflectionPrompt> {
+    try {
+      // Query to fetch the next prompt after the current one
+      const nextPrompt = await this.srp_repository
+        .createQueryBuilder('srp')
+        .where('srp.id > :currentPromptId', { currentPromptId })
+        .orderBy('srp.id', 'ASC')
+        .getOne();
+
+      if (!nextPrompt) {
+        // If no next prompt exists (we are at the last prompt), fetch the first one
+        const firstPrompt = await this.srp_repository
+          .createQueryBuilder('srp')
+          .orderBy('srp.id', 'ASC') // Fetch the first prompt
+          .getOne();
+
+        if (!firstPrompt) {
+          throw new Error('No reflection prompts available');
+        }
+
+        return firstPrompt;
+      }
+
+      return nextPrompt;
+    } catch (error: any) {
+      throw new Error(`Error fetching next prompt: ${error.message}`);
+    }
+  }
+
+  private async createPostFromPrompt(
+    user: User,
+    prompt: StaticReflectionPrompt,
+    createdAt: Date,
+  ): Promise<Post> {
+    const post = new Post();
+    post.user = user;
+    post.input_type = 'TEXT';
+    post.prompt_type = 'REFLECTION';
+    post.status = 'UNANSWERED';
+    post.title = prompt.txt;
+    post.show_order_date = createdAt;
+    post.last_update = createdAt;
+    post.created_at = createdAt;
+    return post;
+  }
+
+  public async getReflectionPrompt(user: User): Promise<Post> {
+    const currentDate = new Date();
+    let expiredPrompt: TrackReflectionPrompt | null;
+
+    try {
+      expiredPrompt = await this.trp_repository.findOne({
+        where: { user, status: 'ACTIVE' },
+      });
+
+      if (expiredPrompt) {
+        const promptDate = new Date(expiredPrompt.created_at);
+        const isExpired =
+          currentDate.getTime() - promptDate.getTime() >
+          PromptHandler.EXPIRATION_DAYS * 24 * 60 * 60 * 1000;
+
+        if (isExpired) {
+          const nextPrompt = await this.getNextReflectionPrompt(
+            expiredPrompt.prompt_id,
+          );
+          const newTrackPrompt = new TrackReflectionPrompt();
+          newTrackPrompt.prompt_id = nextPrompt.id;
+          newTrackPrompt.status = 'ACTIVE';
+          newTrackPrompt.user = user;
+          newTrackPrompt.created_at = currentDate;
+
+          await this.trp_repository.update(
+            { status: 'ACTIVE' },
+            newTrackPrompt,
+          );
+
+          return this.createPostFromPrompt(user, nextPrompt, currentDate);
+        }
+
+        const prompt = await this.srp_repository.findOne({
+          where: { id: expiredPrompt.prompt_id },
+        });
+        if (!prompt) throw new Error('Prompt does not exist');
+
+        return this.createPostFromPrompt(user, prompt, promptDate);
+      }
+
+      const prompt = await this.srp_repository.findOne({
+        order: { id: 'ASC' },
+      });
+      if (!prompt) throw new Error('No prompts available');
+
+      const track_reflection_prompt = new TrackReflectionPrompt();
+      track_reflection_prompt.prompt_id = prompt.id;
+      track_reflection_prompt.status = 'ACTIVE';
+      track_reflection_prompt.user = user;
+      track_reflection_prompt.created_at = currentDate;
+
+      await this.trp_repository.save(track_reflection_prompt);
+
+      return this.createPostFromPrompt(user, prompt, currentDate);
+    } catch (error: any) {
+      throw new Error(`Error processing reflection prompt: ${error.message}`);
+    }
+  }
+
+  public async getAllPrompts(user: User): Promise<Post[]> {
+    try {
+      const familyPrompts = await this.getFamilyPrompts(user);
+      const childrenPrompts = await this.getChildrenPrompts(user);
+      return [...familyPrompts, ...childrenPrompts];
+    } catch (error: any) {
+      throw new Error(`Error fetching all prompts: ${error.message}`);
+    }
+  }
+}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -4,57 +4,61 @@ import { Connection, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { Child } from 'src/entities/child.entity';
+import { PromptHandler } from '../prompt/handler.service';
+import { Post } from 'src/entities/post.entity';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectRepository(User) private readonly userRepository: Repository<User>,
-        @InjectRepository(Child) private readonly childRepository: Repository<Child>,
-        private readonly connection: Connection, // Injecting connection
-      ) {}
-    
-    async signUp(signUpData: SignUpDto): Promise<User> {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    @InjectRepository(Child)
+    private readonly childRepository: Repository<Child>,
+    private readonly connection: Connection,
+    private readonly promptHandler: PromptHandler,
+  ) {}
+
+  async signUp(signUpData: SignUpDto): Promise<User> {
     const queryRunner = this.connection.createQueryRunner();
     await queryRunner.startTransaction();
-    
+
     try {
-        // Create the User entity
-        const user = this.userRepository.create({
+      // Create the User entity
+      const user = this.userRepository.create({
         name: signUpData.name,
         email: signUpData.email,
         password: signUpData.password,
         relation: signUpData.relation,
-        });
-    
-        // Save the User entity first
-        await queryRunner.manager.save(user);
-    
-        // Create Child entities and associate them with the User
-        const children = signUpData.children.map((child) =>
+      });
+
+      // Save the User entity first
+      await queryRunner.manager.save(user);
+
+      // Create Child entities and associate them with the User
+      const children = signUpData.children.map((child) =>
         this.childRepository.create({
-            name: child.name,
-            date_of_birth: child.date_of_birth,
-            gender: child.gender,
-            parent_user: user,
+          name: child.name,
+          date_of_birth: child.date_of_birth,
+          gender: child.gender,
+          parent_user: user,
         }),
-        );
-    
-        // Save the Child entities
-        await queryRunner.manager.save(children);
-    
-        // Commit the transaction
-        await queryRunner.commitTransaction();
+      );
 
-        return user;
+      // Save the Child entities
+      await queryRunner.manager.save(children);
+      user.children = children;
+      // Generate and save dynamic prompts
+      const posts = await this.promptHandler.getAllPrompts(user);
+      console.log('posts: ', posts[0]);
+      await queryRunner.manager.save(Post, posts);
+
+      await queryRunner.commitTransaction();
+
+      return user;
     } catch (error) {
-        // Rollback the transaction in case of an error
-        await queryRunner.rollbackTransaction();
-        throw error;
+      await queryRunner.rollbackTransaction();
+      throw error;
     } finally {
-        // Release the query runner to free up resources
-        await queryRunner.release();
+      await queryRunner.release();
     }
-    }
-
-
+  }
 }
