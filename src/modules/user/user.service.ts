a4fe3b867348b3aa0886c0656d3b0d03ac992b35
chandandass <cdas99633@gmail.com>
2025-01-20 04:47:30 +0530

    formatting applied

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -7,54 +7,53 @@ import { Child } from 'src/entities/child.entity';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectRepository(User) private readonly userRepository: Repository<User>,
-        @InjectRepository(Child) private readonly childRepository: Repository<Child>,
-        private readonly connection: Connection, // Injecting connection
-      ) {}
-    
-    async signUp(signUpData: SignUpDto): Promise<User> {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    @InjectRepository(Child)
+    private readonly childRepository: Repository<Child>,
+    private readonly connection: Connection, // Injecting connection
+  ) {}
+
+  async signUp(signUpData: SignUpDto): Promise<User> {
     const queryRunner = this.connection.createQueryRunner();
     await queryRunner.startTransaction();
-    
+
     try {
-        // Create the User entity
-        const user = this.userRepository.create({
+      // Create the User entity
+      const user = this.userRepository.create({
         name: signUpData.name,
         email: signUpData.email,
         password: signUpData.password,
         relation: signUpData.relation,
-        });
-    
-        // Save the User entity first
-        await queryRunner.manager.save(user);
-    
-        // Create Child entities and associate them with the User
-        const children = signUpData.children.map((child) =>
+      });
+
+      // Save the User entity first
+      await queryRunner.manager.save(user);
+
+      // Create Child entities and associate them with the User
+      const children = signUpData.children.map((child) =>
         this.childRepository.create({
-            name: child.name,
-            date_of_birth: child.date_of_birth,
-            gender: child.gender,
-            parent_user: user,
+          name: child.name,
+          date_of_birth: child.date_of_birth,
+          gender: child.gender,
+          parent_user: user,
         }),
-        );
-    
-        // Save the Child entities
-        await queryRunner.manager.save(children);
-    
-        // Commit the transaction
-        await queryRunner.commitTransaction();
+      );
 
-        return user;
+      // Save the Child entities
+      await queryRunner.manager.save(children);
+
+      // Commit the transaction
+      await queryRunner.commitTransaction();
+
+      return user;
     } catch (error) {
-        // Rollback the transaction in case of an error
-        await queryRunner.rollbackTransaction();
-        throw error;
+      // Rollback the transaction in case of an error
+      await queryRunner.rollbackTransaction();
+      throw error;
     } finally {
-        // Release the query runner to free up resources
-        await queryRunner.release();
+      // Release the query runner to free up resources
+      await queryRunner.release();
     }
-    }
-
-
+  }
 }
