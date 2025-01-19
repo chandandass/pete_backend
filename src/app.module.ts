f1d01df537e821e6eafc5d9c32aef21b934466f5
chandandass <cdas99633@gmail.com>
2025-01-20 04:44:07 +0530

    revert app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-// import { ConfigModule } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { UserModule } from './modules/user/user.module';
@@ -9,18 +9,21 @@ import { TimelineModule } from './modules/timeline/timeline.module';
 @Module({
   imports: [
     // Load configuration from .env
-    // ConfigModule.forRoot({
-    //   isGlobal: true,
-    // }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '1234',
-      database: 'pete_db',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? +process.env.DB_PORT : 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [join(__dirname, '**/**/*.entity{.ts,.js}')],
       synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
 
     // import local modules
@@ -32,17 +35,3 @@ import { TimelineModule } from './modules/timeline/timeline.module';
   providers: [],
 })
 export class AppModule {}
-
-// {
-//   type: 'postgres',
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT ? +process.env.DB_PORT : 5432,
-//   username: process.env.DB_USERNAME,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_DATABASE,
-//   entities: [join(__dirname, '**/**/*.entity{.ts,.js}')],
-//   synchronize: true,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// }
